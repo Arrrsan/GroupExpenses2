be4df32541b92fe61f514b13378e273891d8a594
Arrrsan <157636618+Arrrsan@users.noreply.github.com>
2025-05-08 13:42:30 -0700

    Updated XML files

--- app/src/main/java/com/example/myapplication/CreatingGroupActivity.java
+++ app/src/main/java/com/example/myapplication/CreatingGroupActivity.java
@@ -13,5 +13,12 @@ public class CreatingGroupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_creating_group);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_creating_group), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
